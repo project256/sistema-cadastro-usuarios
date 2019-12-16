b835adf81129085a98d12f5659ad4111973d1c2a
Rodolfo Dirack <programacao@aquainterativa.com.br>
2019-12-16 17:25:10 -0300

    Adiciona cabeçalho ao header.php
    O Script header.php é o cabeçalho das páginas do site.

--- includes/body/header.php
+++ includes/body/header.php
@@ -1,3 +1,24 @@
+<?php
+/*
+	 header.php (PHP)
+	 
+	 Objetivo: Cabeçalho da página e menu de navegação do site.
+	 
+	 Site: https://github.com/project-256-web/project-256
+	 
+	 Versão 1.0
+	 
+	 Programador: 	Gabriel Alves 16/12/2019 (Original - PHP)
+	 
+	 Email (Manutenção): rodolfo_profissional@hotmail.com
+	 
+	 Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+	 
+	 
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
