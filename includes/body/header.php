9ed18fb0579be04c9e01acfb6529091cda0ef7c9
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-12-16 17:11:48 -0300

    Corrige as tags de fechamento link
    Adiciona barras as tags de fechamento link

--- includes/body/header.php
+++ includes/body/header.php
@@ -3,8 +3,8 @@
 
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/logo/apple-icon.png">
-  <link rel="icon" type="image/png" href="./assets/img/logo/favicon.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/logo/apple-icon.png" />
+  <link rel="icon" type="image/png" href="./assets/img/logo/favicon.png" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
     Project 256
@@ -12,7 +12,7 @@
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
   <!-- CSS Files -->
   <link href="./assets/css/material-kit.css" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
