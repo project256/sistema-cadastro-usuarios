fe875c921b1c586fd546170f94065c213f76dfe6
Rodolfo Dirack <rodolfo_profissional@hotmailcom>
2019-12-24 00:05:48 -0300

    Função de atualização do perfil do usuário
    Substitui os ifs do script de atualização de perfil por uma função de
    atualização 'updateUserProfile'. Esta recebe oque será mudado (variável
    var), o valor novo (na variável value) o id do usuário e a conexão pdo.

--- assets/php/atulizarPerfil.php
+++ assets/php/atulizarPerfil.php
@@ -1,84 +1,60 @@
 <?php
 require('connect.php');
-$nameIsSet = (!empty($_POST['nome']) && isset($_POST['nome']));
-$sobreIsSet = (!empty($_POST['sobre']) && isset($_POST['sobre']));
-$passIsSet = (!empty($_POST['senha']) && isset($_POST['senha']));
-$userIsSet = (!empty($_POST['usuario']) && isset($_POST['usuario']));
-$mailIsSet = (!empty($_POST['email']) && isset($_POST['email']));
-
 $id = $_SESSION['id'];
 
-//Trocar o nome
+$nameIsSet = (!empty($_POST['nome']) && isset($_POST['nome']));
+$sobrenomeIsSet = (!empty($_POST['sobre']) && isset($_POST['sobre']));
+$passwordIsSet = (!empty($_POST['senha']) && isset($_POST['senha']));
+$userIsSet = (!empty($_POST['usuario']) && isset($_POST['usuario']));
+$emailIsSet = (!empty($_POST['email']) && isset($_POST['email']));
+
+function updateUserProfile($var,$value,$id,$pdo){
+
+	try {
+		$sql = "UPDATE loginSiteDirack set $var = '$value' where id = '$id'";
+		echo $sql;
+        	$sql = $pdo->query($sql) or die();
+
+    	} catch (PDOExcpetion $e) {
+        	echo "ERRO: ".$e->getMessage();
+        	exit;
+    	}
+}
+
+
 if($nameIsSet){
-    $NOME = $_POST['nome'];
-    
-    try {
-        $sql = "UPDATE loginsitedirack set nome = '$NOME' where id = '$id'";
-        $sql = $pdo->query($sql) or die();
-        $_SESSION['nome'] = $NOME;
+    $value = $_POST['nome'];
+    $var = 'nome';
+    updateUserProfile($var,$value,$id,$pdo);
+    $_SESSION['nome'] = $value;
+} 
 
-    } catch (PDOExcpetion $e) {
-        echo "ERRO: ".$e->getMessage();
-        exit;
-    }
-    
-}
-//Trocar o sobrenome
-if($sobreIsSet){
-    $SOBRE = $_POST['sobre'];
-    
-    try {
-        $sql = "UPDATE loginsitedirack set sobrenome = '$SOBRE' where id = '$id'";
-        $sql = $pdo->query($sql) or die();
-        $_SESSION['sobrenome'] = $SOBRE;
+if($sobrenomeIsSet){
+    $value = $_POST['sobre'];
+    $var = 'sobrenome'; 
+    updateUserProfile($var,$value,$id,$pdo);
+    $_SESSION['sobrenome'] = $value;
+} 
 
-    } catch (PDOExcpetion $e) {
-        echo "ERRO: ".$e->getMessage();
-        exit;
-    }
-    
-}
-//Trocar a senha
-if($passIsSet){
-    $SENHA   = md5($_POST['senha']);
+if($passwordIsSet){
+	$value = md5($_POST['senha']);
+	$var = 'senha';
+	updateUserProfile($var,$value,$id,$pdo);
+	$_SESSION['senha'] = $value;
+} 
 
-    try {
-        $sql = "UPDATE loginsitedirack set senha = '$SENHA' where id = '$id'";
-        $sql = $pdo->query($sql) or die();
-        $_SESSION['senha'] = $SENHA;
-
-    } catch (PDOExcpetion $e) {
-        echo "ERRO: ".$e->getMessage();
-        exit;
-    }
-}
-//Trocar o usuário
 if($userIsSet){
-    $USER  = $_POST['usuario'];
+	$value  = $_POST['usuario'];
+	$var = 'usuario';
+	updateUserProfile($var,$value,$id,$pdo);
+	$_SESSION['usuario'] = $value;
+} 
 
-    try {
-        $sql = "UPDATE loginsitedirack set usuario = '$USER' where id = '$id'";
-        $sql = $pdo->query($sql) or die();
-        $_SESSION['usuario'] = $USER;
-
-    } catch (PDOExcpetion $e) {
-        echo "ERRO: ".$e->getMessage();
-        exit;
-    }
-}
-//Trocar o Email
-if($mailIsSet){
-    $EMAIL  = $_POST['email'];
-
-    try {
-        $sql = "UPDATE loginsitedirack set email = '$EMAIL' where id = '$id'";
-        $sql = $pdo->query($sql) or die();
-        $_SESSION['email'] = $EMAIL;
-
-    } catch (PDOExcpetion $e) {
-        echo "ERRO: ".$e->getMessage();
-        exit;
-    }
+if($emailIsSet){
+    $value  = $_POST['email'];
+	$var = 'email';
+	updateUserProfile($var,$value,$id,$pdo);
+	$_SESSION['email'] = $value;
 }
 
 
