c8c948c8b787796ef5912e97e7b5d3899dd7697b
Rodolfo Dirack <rodolfo_profissional@hotmailcom>
2020-01-02 12:39:53 -0300

    Resolve issue #5
    Corrige link gerado pela página sessionstart.php inclusa no header de
    Page.php

--- includes/funcions/sessionstart.php
+++ includes/funcions/sessionstart.php
@@ -23,7 +23,7 @@ $userIsNotLogged = (empty($_SESSION['logado']) || !isset($_SESSION['logado']));
  
 if($idIsNotSet && $userIsNotLogged){
 	echo "Você precisa estar logado para acessar esta página!<br>";
-	echo "<a href='../../index.php'>Voltar</a>";
+	echo "<a href='index.php'>Voltar</a>";
 	exit;
 }else{
 
