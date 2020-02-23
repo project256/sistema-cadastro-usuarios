2387d171951785b1ebda8013146c6a670793e69b
Dirack <rodolfo_profissional@hotmailcom>
2020-02-23 15:01:03 -0300

    CSS formulário de atualização de perfil Resolve #9
    Adiciona o estio CSS ao formulário de atualização de perfil da página
    'editarPerfil.php' e Resolve #9

--- assets/php/editarPerfil.php
+++ assets/php/editarPerfil.php
@@ -3,16 +3,18 @@ require('../../includes/funcions/sessionstart.php');
 $ROOT_PATH=$_SERVER['DOCUMENT_ROOT']."$PATH";
 require("$ROOT_PATH/includes/body/header.php");
 ?>
+<link type="text/css" rel="stylesheet" href="editarPerfil.css">
 <script src="./js/script.js"></script>
-<form action="atulizarPerfil.php" method="POST" autocomplete="off">
+<form id="formEditarPerfil" action="atulizarPerfil.php" method="POST" autocomplete="off">
+<div id="tituloForm"><h3><strong>Atualização de Perfil de usuário</strong></h3></div><br>
+Digite novos valores para os campos que você deseja mudar:<br>
+<input type="text" name="userChanges['nome']" placeholder="Digite um novo Nome"><br>
+<input type="text" name="userChanges['sobrenome']" placeholder="Digite um novo Sobrenome"><br>
+<input type="text" name="userChanges['usuario']" placeholder="Digite um novo Usuário"><br>
+<input type="email" name="userChanges['email']"  placeholder="Digite um novo Email"><br>
+<input type="password" name="userChanges['senha']" placeholder="Digite uma nova Senha"><br>
 
-<input type="text" name="userChanges['nome']" placeholder="Trocar Nome"><br>
-<input type="text" name="userChanges['sobrenome']" placeholder="Trocar Sobrenome"><br>
-<input type="text" name="userChanges['usuario']" placeholder="Trocar Usuário"><br>
-<input type="email" name="userChanges['email']"  placeholder="Trocar Email"><br>
-<input type="password" name="userChanges['senha']" placeholder="Trocar Senha"><br>
-
-    <input type="submit" value="Trocar">
+    <input type="submit" value="Atualizar">
 </form>
 
 <?php
