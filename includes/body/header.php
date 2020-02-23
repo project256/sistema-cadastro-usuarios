c30727838f70390987ef296fd105aefdd95f1620
Dirack <rodolfo_profissional@hotmailcom>
2020-02-23 13:14:26 -0300

    Adiciona variável PATH ao arquivo header.php #9
    Os caminhos do arquivo 'header.php' agora são descritos com o auxílio da
    variável de sessão PATH que armazena o caminho do diretório raíz do
    site.
    
    Esta variável é referenciada no commit 90780c578e9dfe92e6345677ecf438267fa20647

--- includes/body/header.php
+++ includes/body/header.php
@@ -24,8 +24,8 @@
 
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/Logo/apple-icon.png" />
-  <link rel="icon" type="image/png" href="./assets/img/Logo/favicon.png" />
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $PATH;?>/assets/img/Logo/apple-icon.png" />
+  <link rel="icon" type="image/png" href="<?php echo $PATH;?>/assets/img/Logo/favicon.png" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
     Project 256
@@ -35,18 +35,18 @@
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
   <!-- CSS Files -->
-  <link href="./assets/css/material-kit.css" rel="stylesheet" />
+  <link href="<?php echo $PATH;?>/assets/css/material-kit.css" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="./assets/demo/demo.css" rel="stylesheet" />
-  <link rel="stylesheet" href="./assets/css/modal.css">
-  <link rel="stylesheet" href="./assets/css/estilo.css">
+  <link href="<?php echo $PATH;?>/assets/demo/demo.css" rel="stylesheet" />
+  <link rel="stylesheet" href="<?php echo $PATH;?>/assets/css/modal.css">
+  <link rel="stylesheet" href="<?php echo $PATH;?>/assets/css/estilo.css">
 </head>
 
 <body>
 <nav class="navbar navbar-expand-lg bg-primary">
     <div class="container">
       <div class="navbar-translate">
-        <a class="navbar-brand" href="#0"><img src="./assets/img/Logo/logo.png"></img></a>
+        <a class="navbar-brand" href="#0"><img src="<?php echo $PATH;?>/assets/img/Logo/logo.png"></img></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="sr-only">Navegação</span>
           <span class="navbar-toggler-icon"></span>
@@ -62,7 +62,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="./perfil.php" class="nav-link">
+            <a href="<?php echo $PATH;?>/perfil.php" class="nav-link">
               <i class="material-icons">account_circle</i> Perfil
             </a>
           </li>
@@ -71,9 +71,9 @@
                   <i class="material-icons">settings</i>Configurações
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
-                  <a class="dropdown-item" href="assets/php/editarPerfil.php">Editar Perfil</a>
+                  <a class="dropdown-item" href="<?php echo $PATH;?>/assets/php/editarPerfil.php">Editar Perfil</a>
                   <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href='assets/php/encerraSessao.php'>Sair</a></div>
+                  <a class="dropdown-item" href='<?php echo $PATH;?>/assets/php/encerraSessao.php'>Sair</a></div>
           </li>
         </ul>
       </div>
