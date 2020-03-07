b470b8d853683e73c7bb9949cce35a0951318e5d
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-03-07 00:47:29 -0300

    Importa o sistema de chat no corpo da página #21
    Inclui o arquivo com o sistema de chat no corpo da página de sessão do
    usuário

--- includes/body/body.php
+++ includes/body/body.php
@@ -17,4 +17,6 @@
 
     </div>
     </div>
+    <script src="https://use.fontawesome.com/releases/v5.12.1/js/all.js" data-auto-replace-svg="nest"></script>
+    <?php include 'includes/Chat/index.php';?>    
 </div>
