a6472374a1ca4b00ad4f7a3d74abd45a7d2a47b8
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-03-11 13:31:14 -0300

    Caixa de mensagens do chat em um objeto IFRAME #22
    Insere um objeto iframe para armazenar a caixa de mensagens do sistema
    de chat. Este objeto é gerado na página chatFrame.php e carregado na
    página includes/Chat/index.php dinamicamente.
    
    A idéia do sistema é carregar uma página chatFrame.php para cada usuário
    e conversa quando o sistema de chat for aberto, a partir das conversas
    no banco de dados a página é gerada automaticamente e carregada no
    sistema de chat.

---
+++ includes/Chat/chatFrame.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<!--
+	chatFrame.php (PHP)
+	
+	Objetivo: Iframe com as conversas da caiza de diálogo do Chat.
+	
+	Site: http://www.dirackslounge.online
+	
+	Versão 1.0
+	
+	Programador: Rodolfo A C Neves (Dirack) 11/03/2020
+	
+	Email: rodolfo_profissional@hotmail.com
+	
+	Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+-->
+            <div class="me">
+               <img width="30" height="30" src="<?php echo $PATH;?>/assets/userImages/15764828375df73815de2f1.jpg" alt=""> <span>Olá</span>
+            </div>
+            <div class="other">
+                <span>Salve programador !</span>
+            </div>
+            <div class="me">
+               <img width="30" height="30" src="<?php echo $PATH;?>/assets/userImages/15764828375df73815de2f1.jpg" alt=""> <span>Salveeeeeeeeeeeeeeeeeeeeee</span>
+            </div>
+            <div class="me">
+               <img width="30" height="30" src="<?php echo $PATH;?>/assets/userImages/15764828375df73815de2f1.jpg" alt=""> <span>Brabo</span>
+            </div>
+            <div class="other">
+                <span>Tudo bom ?</span>
+            </div>
+            <div class="other">
+                <span>Qual a boa ?</span>
+            </div>
+
+<?php
+
+?>
